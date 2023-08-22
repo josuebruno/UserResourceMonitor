2e85fa9c1ea7844c1a5692f196fffd7a23b5bb18
josue.bruno <josue.bruno@outlook.com.br>
2023-08-22 00:43:06 -0300

    novo exemplo de consumo.

--- php/index.php
+++ php/index.php
@@ -1,22 +1,68 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <style>
-        .user-container {
-            border: 1px solid #ccc;
-            padding: 10px;
-            margin-bottom: 10px;
-        }
-    </style>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 </head>
 <body>
+
+<?php
+$apiUrl = 'http://172.18.250.28:5000/servers/serv2';
+$response = file_get_contents($apiUrl);
+
+if ($response !== false) {
+    $data = json_decode($response, true);
+    if ($data === null) {
+        echo "Erro ao decodificar JSON.";
+    }
+} else {
+    echo "Erro ao obter resposta da API.";
+}
+?>
+
+<div class="container">
     <?php foreach ($data as $item) : ?>
-        <div class="user-container">
-            <p><strong>Usuário:</strong> <?= $item['username'] ?></p>
-            <p><strong>Percentual de CPU:</strong> <?= $item['cpu_percent'] ?>%</p>
-            <p><strong>Percentual de Memória:</strong> <?= $item['memory_percent'] ?>%</p>
-            <p><strong>Uso de Memória:</strong> <?= $item['memory_usage_bytes'] ?> bytes</p>
+        <div class="card mb-3">
+            <div class="card-body">
+                <h5 class="card-title">Usuário: <?= $item['username'] ?></h5>
+                <p class="card-text">Percentual de CPU: <?= max(0, $item['cpu_percent']) ?>%</p>
+                <p class="card-text">Percentual de Memória: <?= max(0, $item['memory_percent']) ?>%</p>
+                <p class="card-text">Uso de Memória: <?= round(max(0, $item['memory_usage_bytes']) / (1024 * 1024), 2) ?> MB</p>
+                <div id="cpuChart<?= $item['username'] ?>"></div>
+                <div id="memoryChart<?= $item['username'] ?>"></div>
+            </div>
         </div>
+
+        <script>
+            google.charts.load('current', {'packages':['corechart']});
+            google.charts.setOnLoadCallback(drawCharts<?= $item['username'] ?>);
+
+            function drawCharts<?= $item['username'] ?>() {
+                var data = google.visualization.arrayToDataTable([
+                    ['Task', 'Percentual'],
+                    ['CPU', <?= max(0, $item['cpu_percent']) ?>],
+                    ['Memória', <?= max(0, $item['memory_percent']) ?>]
+                ]);
+
+                var options = {
+                    title: 'Uso de Recursos',
+                    pieHole: 0.4,
+                    colors: ['#2196F3', '#F44336']
+                };
+
+                var cpuChart<?= $item['username'] ?> = new google.visualization.PieChart(document.getElementById('cpuChart<?= $item['username'] ?>'));
+                cpuChart<?= $item['username'] ?>.draw(data, options);
+
+                var memoryChart<?= $item['username'] ?> = new google.visualization.PieChart(document.getElementById('memoryChart<?= $item['username'] ?>'));
+                memoryChart<?= $item['username'] ?>.draw(data, options);
+            }
+        </script>
     <?php endforeach; ?>
+</div>
+
 </body>
 </html>
